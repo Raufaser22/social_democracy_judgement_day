a67d0d52a4daf28575cd9f20e0eef8194e795d2f
Raufaser22 <Ulrichhaus2@gmail.com>
2026-09-08 17:35:47 +0200

    work on elections

--- out/html/d3-linegraph.js
+++ out/html/d3-linegraph.js
@@ -18,11 +18,11 @@ d3.linegraph = function(noTicks, noDots, parties, partyColors, partyNames, dataM
     if (!parties) {
         parties = ['spd', 'kpd', 'ddp', 'z', 'dvp', 'dnvp', 'nsdap', 'other'];
     }
-    if (!partyColors) {
-        partyColors = {'spd': '#E3000F', 'kpd': '#8B0000', 'ddp': '#DCCA4A', 'z': '#000', 'dvp': '#D5AC27', 'dnvp': '#3f7bc1', 'nsdap': '#954B00', 'other': '#a0a0a0'};
+    if (!partyColors||partyColors) {
+        partyColors = {'spd': '#E3000F', 'kpd': '#BE3075', 'ddp': '#409A3C', 'z': '#4d4d5e', 'dvp': '#D6C80E', 'dnvp': '#00A2DE', 'nsdap': '#954B00', 'other': '#a0a0a0'};
     }
     if (!partyNames) {
-        partyNames = {'spd': 'SPD', 'kpd': 'KPD', 'ddp': 'DDP', 'z': 'Z + BVP', 'dvp': 'DVP', 'dnvp': 'DNVP', 'nsdap': 'NSDAP', 'other': 'Others'};
+        partyNames = {'spd': 'SPD', 'kpd': 'Linke', 'ddp': 'Greens', 'z': 'CDU/CSU', 'dvp': 'FDP', 'dnvp': 'AfD', 'nsdap': 'NSDAP', 'other': 'Others'};
     }
     if (!additionalMonths) {
         additionalMonths = 10;
